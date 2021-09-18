f70ee08338b2590b87e317f7a5550c4363bce4a8
Levi Zitting <me@levizitting.com>
2021-09-18 02:08:56 -0500

    Add .env setup

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const GOOGLE_MAPS_API_KEY = process.env.VUE_APP_GOOGLE_MAPS_API_KEY;
+export const API_BASE_URL = process.env.VUE_APP_API_BASE_URL;
+export const ALGOLIA_APP_ID = process.env.VUE_APP_ALGOLIA_APP_ID;
+export const ALGOLIA_API_KEY = process.env.VUE_APP_ALGOLIA_API_KEY;
