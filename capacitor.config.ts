1d8018c325652c139ae44fac4e29d0b3c0ea8a8f
Levi Zitting <me@levizitting.com>
2021-09-17 23:21:13 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'portal-to-work-client',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
