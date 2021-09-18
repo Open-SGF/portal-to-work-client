cfe8187bd4a8b74ceb15d50c3545d4a833c522ff
McMuffinman11 <liquidbionics@gmail.com>
2021-09-18 16:46:18 -0500

    Add job title, location, date, and paragraph

---
+++ src/components/EventListingItem.vue
@@ -0,0 +1,58 @@
+<template>
+    <ion-item class="">
+        <ion-avatar class="date-wrapper">
+            <div class="date">
+                <span class="month ion-text-uppercase">Nov</span>
+                <span class="day">12</span>
+            </div>
+        </ion-avatar>
+        <ion-label class="text-wrapper">
+            <h2>Job Title</h2>
+            <h3>Location</h3>
+            <p>Lorem Ipsum baln</p>
+        </ion-label>
+    </ion-item>
+</template>
+
+<script>
+import { IonItem, IonAvatar, IonLabel } from '@ionic/vue';
+
+export default {
+    name: 'EventListingItem',
+    components: {
+        IonItem,
+        IonAvatar,
+        IonLabel,
+    },
+};
+</script>
+
+<style scoped>
+.date-wrapper {
+    margin-right: 15px;
+}
+
+.date {
+    background: var(--ion-color-primary);
+    color: var(--ion-color-primary-contrast);
+    border-radius: 50%;
+    height: 40px;
+    width: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 8px;
+    font-size: 12px;
+    margin-right: 15px;
+}
+
+.month {
+    font-size: 11px;
+}
+
+.text-wrapper > h2,
+.text-wrapper > h3,
+.text-wrapper > p {
+    padding: 4px;
+}
+</style>
--- src/views/EventsListing.vue
+++ src/views/EventsListing.vue
@@ -5,21 +5,33 @@
                 <ion-title class="ion-text-center">Events</ion-title>
             </ion-toolbar>
         </ion-header>
-        <ion-content :fullscreen="true"> </ion-content>
+        <ion-content :fullscreen="true">
+            <ion-list v-on:click="foo()">
+                <EventListingItem />
+            </ion-list>
+        </ion-content>
     </ion-page>
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList } from '@ionic/vue';
+import EventListingItem from '../components/EventListingItem.vue';
 
 export default {
-    name: 'eventListing',
+    name: 'EventListing',
     components: {
         IonHeader,
         IonToolbar,
         IonTitle,
         IonContent,
         IonPage,
+        IonList,
+        EventListingItem,
+    },
+    methods: {
+        foo() {
+            console.log('hi mum');
+        },
     },
 };
 </script>
