111fc97f32d50d1022f5c9ff86605d26ab690dcf
Ethan Zitting <ethan@ethanzitting.com>
2021-11-26 08:59:15 -0600

    wip: style search bar with ionic

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,6 +1,18 @@
 <template>
     <ais-instant-search :search-client="searchClient" index-name="test_portal-to-work">
-        <ais-search-box />
+        <ais-search-box class="search-bar">
+            <template v-slot="{ currentRefinement, isSearchStalled, refine }">
+                <ion-searchbar
+                    type="search"
+                    :value="currentRefinement"
+                    @input="refine($event.currentTarget.value)"
+                ></ion-searchbar>
+                <ion-fab-button class="ion-padding" router-link="/jobs/filters">
+                    <ion-icon :icon="filterOutline"></ion-icon>
+                </ion-fab-button>
+                <span :hidden="!isSearchStalled">Loading...</span>
+            </template>
+        </ais-search-box>
         <div class="ion-padding-horizontal">
             <slot></slot>
         </div>
@@ -16,28 +28,27 @@
             </template>
         </ais-hits>
     </ais-instant-search>
+    <div class="search-bar ion-padding-horizontal"></div>
 </template>
 
 <script>
+import { IonSearchbar, IonFabButton } from '@ionic/vue';
+import { filterOutline } from 'ionicons/icons';
 import algoliasearch from 'algoliasearch';
 import { ALGOLIA_APP_ID, ALGOLIA_API_KEY } from '../config';
 import JobItem from '@/components/JobItem.vue';
 
 export default {
-    props: {
-        value: {
-            required: true,
-            type: String,
-        },
-    },
-
     components: {
         JobItem,
+        IonSearchbar,
+        IonFabButton,
     },
 
     data() {
         return {
             searchClient: algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY),
+            filterOutline,
         };
     },
 };
